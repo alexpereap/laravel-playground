349f5b0132ad468cf89e43d7b5863987ef8e1a8a
Alex Perea <ing.alexperea@gmail.com>
2024-11-14 06:51:15 -0500

    adding a middleware

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return 'hole mundo';
 });
+
+Route::get('/user/{id}', function (string $id) {
+    dd($id);
+});
+
+Route::get('nombre/{name?}', function (?string $name = 'Alex') {
+    return $name;
+});
+
+Route::get('middle', function () {
+    echo "middleware";
+})->middleware('admin');
